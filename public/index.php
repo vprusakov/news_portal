3a2aa10aa1a328f5279b98c4025e35045d964ed5
Vyacheslav Prusakov <vyacheslav.prusakov@gmail.com>
2019-04-04 11:04:11 +0300

    [add] index.php with basic structure

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+      <meta charset="utf-8">
+      <title>Новости</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="/css/styles.css" rel="stylesheet">
+  </head>
+  <body>
+  </body>
+</html>
