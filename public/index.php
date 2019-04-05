2ecb22e684e8c6cedcd59b2f13bd985f5798ab56
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-06 01:01:55 +0300

    [update] public/index inits Core/Application

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="ru">
-  <head>
-      <meta charset="utf-8">
-      <title>Новости</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="/css/styles.css" rel="stylesheet">
-  </head>
-  <body>
-  </body>
-</html>
+<?php
+
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('APP', ROOT . 'application' . DIRECTORY_SEPARATOR);
+require ROOT . 'application/Core/application.php';
+$app = new Application();
