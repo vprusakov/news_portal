5657731b16593eb20c6fb23d08d193b921503598
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-09 00:16:31 +0300

    [add] outputs news entries with id and delete link

---
+++ application/view/manager/index.php
@@ -0,0 +1,15 @@
+<?php foreach ($news as $news_item) { ?>
+    <div class="feed__item">
+        <div class="entry">
+            <div class="entry__header">
+                <span class="entry__id">#<?php echo $news_item->id; ?></span>
+                <a href="/manager/delete/<?php echo $news_item->id; ?>" class="entry__delete-link link link--danger floated-right">Удалить</a>
+            </div>
+            <div class="entry__content">
+                <h2><?php echo $news_item->headline; ?></h2>
+                <p><?php echo $news_item->intro; ?></p>
+                <a href="home/read/<?php echo $news_item->id; ?>" class="entry__read-link" ></a>
+            </div>
+        </div>
+    </div>
+<?php } ?>
