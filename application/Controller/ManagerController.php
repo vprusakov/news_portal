e3754f2fbd15b0179ee476a85041ae887b3f11c8
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-09 00:11:26 +0300

    [update] show all news on manager/index

--- application/Controller/ManagerController.php
+++ application/Controller/ManagerController.php
@@ -1,13 +1,16 @@
 <?php
 
 require APP . 'view/_components/Nav.php';
+require APP . "Model/News.php";
 
 class ManagerController
 {
     private $Nav = null;
+    private $News = null;
 
     public function __construct() {
         $this->Nav = new Nav();
+        $this->News = new News();
     }
 
     public function index()
@@ -16,8 +19,10 @@ class ManagerController
 
         echo $this->Nav->createNav('/manager');
 
+        $news = $this->News->getAllNews();
         require APP . 'view/manager/index.php';
         
         require APP . 'view/_templates/footer.php';
     }
+
 }
