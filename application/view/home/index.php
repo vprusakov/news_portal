b6e210cb7c153d28a4ee8ffea5ca2d6cef780d76
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-09 00:15:41 +0300

    [update] users shouldn't see news id

--- application/view/home/index.php
+++ application/view/home/index.php
@@ -1,9 +1,6 @@
 <?php foreach ($news as $news_item) { ?>
     <div class="feed__item">
         <div class="entry">
-            <div class="entry__header">
-                <span class="entry__id">#<?php echo $news_item->id; ?></span>
-            </div>
             <div class="entry__content">
                 <h2><?php echo $news_item->headline; ?></h2>
                 <p><?php echo $news_item->intro; ?></p>
