e42c0da535b36880c518449b8160e86ad84ffb55
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-08 17:51:17 +0300

    [add] now can call controllers' methods with passed args

--- application/Core/application.php
+++ application/Core/application.php
@@ -3,6 +3,8 @@
 class Application
 {
     private $url_controller = null;
+    private $url_action = null;
+    private $url_params = array();
 
     public function __construct()
     {
@@ -16,7 +18,16 @@ class Application
             require APP . 'Controller/' . ucfirst($this->url_controller) . 'Controller.php';
             $controller_name = ucfirst($this->url_controller) . 'Controller';
             $controller = new $controller_name();
-            $controller->index();
+
+            if (method_exists($controller, $this->url_action)) {
+                if (!empty($this->url_params)) {
+                    call_user_func_array(array($controller, $this->url_action), $this->url_params);
+                } else {
+                    $this->url_controller->{$this->url_action}();
+                }
+            } else {
+                $controller->index();
+            }
         }
     }
 
@@ -26,6 +37,9 @@ class Application
             $url = trim($_GET['url'], '/');
             $url = explode('/', $url);
             $this->url_controller = isset($url[0]) ? $url[0] : null;
+            $this->url_action = isset($url[1]) ? $url[1] : null;
+            unset($url[0], $url[1]);
+            $this->url_params = array_values($url);
         }
     }
 }
