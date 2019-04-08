d843f130ced26a67ed39e5625da73d4cb3a3659d
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-08 17:49:22 +0300

    [add] read(id) lets you read an article by id

--- application/Controller/HomeController.php
+++ application/Controller/HomeController.php
@@ -4,16 +4,31 @@ require APP . "Model/News.php";
 
 class HomeController
 {
+    private $News = null;
+
+    public function __construct() {
+        $this->News = new News();
+    }
+
     public function index()
     {
         require APP . 'view/_templates/header.php';
-        
         require APP . 'view/_components/nav.php';
         echo createNav('/');
 
-        $News = new News();
-        $news = $News->getAllNews();
+        $news = $this->News->getAllNews();
         require APP . 'view/home/index.php';
+
+        require APP . 'view/_templates/footer.php';
+    }
+
+    public function read($id) {
+        require APP . 'view/_templates/header.php';
+        require APP . 'view/_components/nav.php';
+        echo createNav('/manager');
+
+        $news_entry = $this->News->getNewsEntryById($id)[0];
+        require APP . 'view/home/read.php';
 
         require APP . 'view/_templates/footer.php';
     }
