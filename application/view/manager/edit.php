7db48e9632d80a49db1477db5f2385f241471083
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-11 12:44:03 +0300

    [add] editing ui with script element

---
+++ application/view/manager/edit.php
@@ -0,0 +1,12 @@
+<div class="entry">
+    <div class="entry__body" id="editor">
+        <h2 data-name="headline" contenteditable id="js-headline"><?php echo $news_entry->headline; ?></h2>
+        <div data-name="intro" contenteditable id="js-intro"><?php echo $news_entry->intro; ?></div>
+        <div data-name="content" contenteditable id="js-content"><?php echo $news_entry->content; ?></div>
+    </div>
+    <div class="entry__footer">
+        <a href="/manager/update/<?php echo $news_entry->id; ?>" class="link" id="js-save">Сохранить</a>
+    </div>
+</div>
+<script src="/js/app.js"></script>
+<script>processEdits.init('/manager/update/<?php echo $news_entry->id; ?>')</script>
