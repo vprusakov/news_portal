a7a0d028aa34ccb51bd42a4104646750053b2181
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-11 12:40:23 +0300

    [update] getNewsEntryById() return entry row with intro field

--- application/Model/News.php
+++ application/Model/News.php
@@ -14,7 +14,7 @@ class News extends Model
     }
 
     public function getNewsEntryById($id) {
-        $sql = "SELECT id, headline, content FROM news WHERE id=$id";
+        $sql = "SELECT id, headline, intro, content FROM news WHERE id=$id";
         $query = $this->db->prepare($sql);
         $query->execute();
         return $query->fetchAll();    
@@ -28,6 +28,12 @@ class News extends Model
     
     public function addNewsEntry($headline, $intro, $content) {
         $sql = "INSERT INTO news (headline, intro, content) VALUES ('$headline', '$intro', '$content')";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+    }
+
+    public function updateNewsEntryById($id, $headline, $intro, $content) {
+        $sql = "UPDATE news SET headline = '$headline', intro = '$intro', content = '$content' WHERE id=$id";
         $query = $this->db->prepare($sql);
         $query->execute();
     }
