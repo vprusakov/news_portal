0fb5f1b56fe468b51b047dbf7cf2d8824f4a1be1
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-11 11:26:44 +0300

    [fix] order news by id desc and return nothing when delete

--- application/Model/News.php
+++ application/Model/News.php
@@ -7,13 +7,9 @@ class News extends Model
 {
     public function getAllNews()
     {
-        $sql = "SELECT id, headline, intro FROM news";
+        $sql = "SELECT id, headline, intro FROM news ORDER BY id DESC";
         $query = $this->db->prepare($sql);
         $query->execute();
-        // fetchAll() is the PDO method that gets all result rows, here in object-style because we defined this in
-        // core/controller.php! If you prefer to get an associative array as the result, then do
-        // $query->fetchAll(PDO::FETCH_ASSOC); or change core/controller.php's PDO options to
-        // $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC ...
         return $query->fetchAll();
     }
 
@@ -28,6 +24,5 @@ class News extends Model
         $sql = "DELETE FROM news WHERE id=$id";
         $query = $this->db->prepare($sql);
         $query->execute();
-        return $query->fetchAll();    
     } 
 }
