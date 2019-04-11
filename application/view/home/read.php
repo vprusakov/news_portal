a80f60f72cce7ecea3b915c33f8dd3a0dd670dd9
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-11 12:42:56 +0300

    [fix] div used as container to avoid bugs when new paragraph is added

--- application/view/home/read.php
+++ application/view/home/read.php
@@ -1,6 +1,6 @@
 <div class="entry">
     <div class="entry__content">
         <h2><?php echo $news_entry->headline; ?></h2>
-        <p><?php echo $news_entry->content ?></p>
+        <div><?php echo $news_entry->content ?></div>
     </div>
 </div>
--- application/view/manager/add.php
+++ application/view/manager/add.php
@@ -1,8 +1,8 @@
 <div class="entry">
     <div class="entry__body" id="editor">
         <h2 data-name="headline" contenteditable id="js-headline">Заголовок</h2>
-        <p data-name="intro" contenteditable id="js-intro">Вступление</p>
-        <p data-name="content" contenteditable id="js-content">Текст</p>
+        <div data-name="intro" contenteditable id="js-intro"><p>Вступление</p></div>
+        <div data-name="content" contenteditable id="js-content"><p>Текст</p></div>
     </div>
     <div class="entry__footer">
         <a href="/manager/save" class="link" id="js-save">Сохранить</a>
