3aefa8bcb615839a23bdf85f285390493a811b8f
Vyacheslav <vyacheslav.prusakov@gmail.com>
2019-04-11 11:33:57 +0300

    [add] add.php is a chunk with editable fields and script element

---
+++ application/view/manager/add.php
@@ -0,0 +1,11 @@
+<div class="entry">
+    <div class="entry__body" id="editor">
+        <h2 data-name="headline" contenteditable id="js-headline">Заголовок</h2>
+        <p data-name="intro" contenteditable id="js-intro">Вступление</p>
+        <p data-name="content" contenteditable id="js-content">Текст</p>
+    </div>
+    <div class="entry__footer">
+        <a href="/manager/save" class="link" id="js-save">Сохранить</a>
+    </div>
+</div>
+<script src="/js/app.js"></script>
